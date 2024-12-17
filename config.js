ddbe2963c4ba810719ac359f1b0ef3aa1fd20d00
Seongyoung123123 <Kumela281@gmail.com>
2024-12-18 04:15:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -24,8 +24,8 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 //global.pairingNumber = "" //put your bot number here
 
 global.mods = ['923057092048', '923057092048']
-global.prems = ['2349013432338', '2349013432338', '2349013432338']
-global.allowed = ['2349013432338', '2349013432338', '2349013432338']
+global.prems = ['923057092048', '923057092048', '923057092048']
+global.allowed = ['923057092048', '923057092048', '923057092048']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
